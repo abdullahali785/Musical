b5b0cf81903bfbf3b9a87d419db5c4d788173797
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2025-12-01 19:36:52 -0600

    with app context

--- App/__init__.py
+++ App/__init__.py
@@ -39,28 +39,29 @@ def create_app() -> Flask:
     this_app = Flask(__name__)
     this_dir = pathlib.Path(__file__).parent
 
-    if os.environ.get("RENDER") is None:
-        dotenv.load_dotenv(this_dir / ".flaskenv")
-    this_app.config.from_prefixed_env()
+    with this_app.app_context():
+        if os.environ.get("RENDER") is None:
+            dotenv.load_dotenv(this_dir / ".flaskenv")
+        this_app.config.from_prefixed_env()
 
-    data_file = this_app.config.get("DATA_FILE", "cast") + ".sqlite3"
-    if os.environ.get("RENDER"):
-        db_path = pathlib.Path("/opt/render/project/src/tmp") / data_file
-    else:
-        db_path = this_dir.parent / "data" / data_file
+        data_file = this_app.config.get("DATA_FILE", "cast") + ".sqlite3"
+        if os.environ.get("RENDER"):
+            db_path = pathlib.Path("/opt/render/project/src/tmp") / data_file
+        else:
+            db_path = this_dir.parent / "data" / data_file
 
-    db_path.parent.mkdir(parents=True, exist_ok=True)
+        db_path.parent.mkdir(parents=True, exist_ok=True)
 
-    this_app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
-    this_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        this_app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
+        this_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    db.init_app(this_app)
+        db.init_app(this_app)
 
-    this_app.register_blueprint(view, url_prefix="/view")
-    this_app.register_blueprint(edit, url_prefix="/edit")
+        this_app.register_blueprint(view, url_prefix="/view")
+        this_app.register_blueprint(edit, url_prefix="/edit")
 
-    @this_app.route("/")
-    def root_redirect():
-        return redirect("/view/")
+        @this_app.route("/")
+        def root_redirect():
+            return redirect("/view/")
 
     return this_app
